487b66f9a2a651092a502fd3d9de247782a42960
Jay <jjang3@vt.edu>
2022-11-20 20:27:18 +0000

    Working on the waterfall script

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Waterfall
 Repository for the Waterfall project
+
+## What's included
+
+```
+./
+├── include
+├── sources
+|      ├── 
+|      ├── 
+|      └── 
+├── SVF (submodule)
+└── waterfall.sh
+```
+- `include` = contains header files for the waterfall pass
+- `sources` = contains source files for the waterfall pass
+- `waterfall.sh` = a script that will `waterfall` a source code.
+
+## How to use (To-do)
+1) `git submodule init && git submodule update`
+2) Change `LT_LLVM_INSTALL_DIR` in `CMakeLists.txt` file to the full directory of where the built `LLVM` project exists.
+3) From the root directory, `mkdir build && cd build`
+4) `cmake .. && make`, this will first build the `SVF` library, then `waterfall`.
+5) Insert an input file to `./inputs`
+6) `bash waterfall.sh <source code name> waterfall-pass (e.g., `bash waterfall.sh vuln_srv waterfall-pass`)
--- waterfall.sh
+++ waterfall.sh
@@ -4,36 +4,30 @@
 BASE_DIR=$(pwd)
 LIB_DIR=$BASE_DIR/build
 INPUT_FILE=$1.c
-TESTING_DIR=$BASE_DIR/testing_inputs
-SHROUD_FILENAME=$TESTING_DIR/shroud
-SOURCE_DIR=$TESTING_DIR/sources
-TEST_INPUT_DIR=$TESTING_DIR/$1
-TEST_INPUT_SOURCE_FILE=$SOURCE_DIR/$1.c
+INPUT_DIR=$BASE_DIR/inputs
+INPUT_SOURCE_FILE=$INPUT_DIR/$1.c
+RESULTS_DIR=$BASE_DIR/results
 
-
-echo $BASE_DIR $TESTING_DIR $TEST_INPUT_DIR $TEST_INPUT_SOURCE_FILE
-
-if [ -f "$TEST_INPUT_SOURCE_FILE" ]; then
-  #echo "$TEST_INPUT_SOURCE_FILE does exist"
-  if [ ! -d "$TEST_INPUT_DIR" ]; then
-    mkdir $TEST_INPUT_DIR
-  fi
+#echo $BASE_DIR $TESTING_DIR $TEST_INPUT_DIR $TEST_INPUT_SOURCE_FILE
+if [ ! -d "$RESULTS_DIR" ]; then
+    mkdir $RESULTS_DIR
 fi
 
-if [ ! -f "$SHROUD_FILENAME.c" ]; then
-  echo "Shroud file missing"
-  exit 0
+if [ -f "$INPUT_SOURCE_FILE" ]; then
+  echo "$TEST_INPUT_SOURCE_FILE does exist"
+else then
+  echo "$TEST_INPUT_SOURCE_FILE does not exist"
 fi
 
 
-clang -c -target aarch64-linux-gnu -march=armv8.5-a+memtag+rng -fsanitize=memtag $SHROUD_FILENAME.c -o $TEST_INPUT_DIR/shroud.o
-clang -emit-llvm -S -target aarch64-linux-gnu -march=armv8.5-a+memtag+rng -fsanitize=memtag -o $TEST_INPUT_DIR/$1.ll $TEST_INPUT_SOURCE_FILE
-clang -emit-llvm -c -target aarch64-linux-gnu -march=armv8.5-a+memtag+rng -fsanitize=memtag -o $TEST_INPUT_DIR/$1.bc $TEST_INPUT_SOURCE_FILE
+#clang -c -target aarch64-linux-gnu -march=armv8.5-a+memtag+rng -fsanitize=memtag $SHROUD_FILENAME.c -o $TEST_INPUT_DIR/shroud.o
+#clang -emit-llvm -S -target aarch64-linux-gnu -march=armv8.5-a+memtag+rng -fsanitize=memtag -o $TEST_INPUT_DIR/$1.ll $TEST_INPUT_SOURCE_FILE
+#clang -emit-llvm -c -target aarch64-linux-gnu -march=armv8.5-a+memtag+rng -fsanitize=memtag -o $TEST_INPUT_DIR/$1.bc $TEST_INPUT_SOURCE_FILE
 
-echo "Shroud in progress..."
-sleep 0.5
-echo "opt -load-pass-plugin $LIB_DIR/lib/"lib"$2.so -passes="$2" -S $TEST_INPUT_DIR/$1.bc -o $TEST_INPUT_DIR/"$1"_modified.ll"
-opt -load-pass-plugin $LIB_DIR/lib/"lib"$2.so -passes="$2" -S $TEST_INPUT_DIR/$1.bc -o $TEST_INPUT_DIR/"$1"_modified.ll
+#echo "Shroud in progress..."
+#sleep 0.5
+#echo "opt -load-pass-plugin $LIB_DIR/lib/"lib"$2.so -passes="$2" -S $TEST_INPUT_DIR/$1.bc -o $TEST_INPUT_DIR/"$1"_modified.ll"
+#opt -load-pass-plugin $LIB_DIR/lib/"lib"$2.so -passes="$2" -S $TEST_INPUT_DIR/$1.bc -o $TEST_INPUT_DIR/"$1"_modified.ll
 #clang  -target aarch64-linux-gnu -march=armv8.5-a+memtag+rng -fsanitize=memtag $TEST_INPUT_DIR/"$1"_modified.ll $TEST_INPUT_DIR/shroud.o -o  $TEST_INPUT_DIR/"$1"_shroud.out
 #clang -g -target aarch64-linux-gnu -march=armv8.5-a+memtag+rng -fsanitize=memtag $TEST_INPUT_DIR/$1.ll -o $TEST_INPUT_DIR/"$1"_mte.out
 #clang  -target aarch64-linux-gnu $TEST_INPUT_DIR/$1.ll -o $TEST_INPUT_DIR/$1_orig.out
