a22576f778bb238f3f83387399619403c0954ac3
Jay <jjang3@vt.edu>
2022-12-05 01:10:18 +0000

    Test inputs

---
+++ scripts/execve.sh
@@ -0,0 +1,1 @@
+pin -follow-execv -t libdft-mod.so -s 0 -f 0 -n 1 -- ./execve-test-overflow
---
+++ scripts/nginx.sh
@@ -0,0 +1,1 @@
+pin -follow-execv -t libdft-mod.so -s 0 -f 0 -n 1  -- /home/jay/Downloads/nginx-1.3.9/install_x86_64/sbin/nginx -c /home/jay/Downloads/nginx-1.3.9/install_x86_64/conf/nginx.conf -p /home/jay/Downloads/nginx-1.3.9/install_x86_64     
