b7397d8b106a48095ad1c9d299c8fe0e40436c8f
Jay <jjang3@vt.edu>
2022-12-05 00:32:27 +0000

    Update script

--- taint.sh
+++ taint.sh
@@ -6,3 +6,5 @@ wget https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.20-984
 tar -xvf pin-3.20-98437-gf02b61307-gcc-linux.tar.gz
 mv pin-3.20-98437-gf02b61307-gcc-linux pin-3.20
 rm pin-3.20-98437-gf02b61307-gcc-linux.tar.gz
+
+export PIN_ROOT=$cwd/pin-3.20
