7690ce699aedfee303be0ebb30041f2cf3650094
Jay <jjang3@vt.edu>
2022-12-05 00:30:04 +0000

    Update script

--- taint.sh
+++ taint.sh
@@ -1,5 +1,8 @@
 #!bin/sh
 
+git submodule init && git submodule update
+
 wget https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.20-98437-gf02b61307-gcc-linux.tar.gz
 tar -xvf pin-3.20-98437-gf02b61307-gcc-linux.tar.gz
 mv pin-3.20-98437-gf02b61307-gcc-linux pin-3.20
+rm pin-3.20-98437-gf02b61307-gcc-linux.tar.gz
