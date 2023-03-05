9aa1901fb55f0bb0ed300ee8ba11369f1a62225a
Jae-Won <jjang3@vt.edu>
2023-03-05 13:03:09 -0500

    Streamlining

--- e9stuff/src/trampoline.c
+++ e9stuff/src/trampoline.c
@@ -10,7 +10,7 @@
 #define YELLOW  "\33[33m"
 #define WHITE   "\33[0m"
 
-extern int pkey;
+int pkey;
 /*
  * Entry point.
  *
@@ -19,7 +19,6 @@ extern int pkey;
 void entry(intptr_t static_addr, intptr_t asm_str, const char *entry_exit_flag, intptr_t arg4)
 {
     if (strcmp(entry_exit_flag, "entry") == 0) {
-        pkey++;
         fprintf(stderr, YELLOW "%.16lx: pkey: %d" RED " disable access\n" WHITE, static_addr, pkey, entry_exit_flag);
         pkey_disable_access(); // <- segmentation fault here.
         // uncomment this to enable restriction access
@@ -31,3 +30,12 @@ void entry(intptr_t static_addr, intptr_t asm_str, const char *entry_exit_flag, 
     //printf("This is where pkey will be set/disabled\n");
 }
 
+
+/*
+ * Initialization.
+ */
+void init(int argc, char **argv, char **envp)
+{
+    pkey++;
+    fprintf(stderr, YELLOW "Initialization (incrementing pkey): %d\n" WHITE, pkey);
+}
