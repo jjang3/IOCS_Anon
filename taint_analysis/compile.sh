1356e080addbc18eefe1055d8324830d3654f8b5
Jae-Won <jjang3@vt.edu>
2023-05-14 15:33:45 -0400

    Fixing compile.sh

--- taint_analysis/compile.sh
+++ taint_analysis/compile.sh
@@ -5,12 +5,13 @@ LIBDFT_DIR=$(pwd)/libdft64
 SOURCE_DIR=$(pwd)/libdft64_files
 RESULTS_DIR=$BASE_DIR/lib
 LIB_DIR=$LIBDFT_DIR/tools/obj-intel64
+PIN_VER=3.20
 
 cp $SOURCE_DIR/makefile.rules $LIBDFT_DIR/tools/ 
 cp $SOURCE_DIR/libdft-OR.cpp $LIBDFT_DIR/tools/
 cp $SOURCE_DIR/libdft-mod.cpp $LIBDFT_DIR/tools/
 cp $SOURCE_DIR/libdft-default.cpp $LIBDFT_DIR/tools/
-cd $LIBDFT_DIR/tools/ && make DEBUG=1 -j4
+cd $LIBDFT_DIR/tools/ && export PIN_ROOT=$BASE_DIR/pin-"$PIN_VER"_build && echo $PIN_ROOT && make DEBUG=1
 
 if [ ! -d "$RESULTS_DIR" ]; then
     mkdir $RESULTS_DIR
