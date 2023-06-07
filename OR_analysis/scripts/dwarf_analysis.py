f7ae8724d4ea0a29b540e2e5512e3fcba8c07bda
Jae-Won <jjang3@vt.edu>
2023-06-07 13:06:37 -0400

    DWARF analysis updated with bninja

--- OR_analysis/scripts/dwarf_analysis.py
+++ OR_analysis/scripts/dwarf_analysis.py
@@ -1,18 +1,83 @@
 from __future__ import print_function
+from binaryninja import *
 import sys
+import re
+import pprint
 
 # If pyelftools is not installed, the example can also run from the root or
 # examples/ dir of the source distribution.
 sys.path[0:0] = ['.', '..']
 
+sys.path.insert(0, '/home/jaewon/binaryninja')
+
 from elftools.elf.elffile import ELFFile
+from elftools.dwarf.dwarf_expr import DWARFExprParser, DWARFExprOp
 from elftools.dwarf.descriptions import (
     describe_DWARF_expr, set_global_machine_arch)
 from elftools.dwarf.locationlists import (
     LocationEntry, LocationExpr, LocationParser)
 from elftools.dwarf.descriptions import describe_form_class
+from elftools.dwarf.callframe import (
+    CallFrameInfo, CIE, FDE, instruction_name, CallFrameInstruction,
+    RegisterRule, DecodedCallFrameTable, CFARule)
+from elftools.dwarf.structs import DWARFStructs
+from elftools.dwarf.descriptions import (describe_CFI_instructions,
+    set_global_machine_arch)
+from elftools.dwarf.enums import DW_EH_encoding_flags
 
+def create_mem_inst_token(input):
+    inst = ""
+    inst += "mov dword [" + input + "], 0x*"
+    print(inst)
+    return inst
+
+bninja_fun_insts = dict() # dictionary for binary ninja analysis
+dwarf_frame_bases = dict() # dictionary for DWARF analysis which contains addr_range, expr per fun.
+dwarf_var_info = dict() # dictionary for DWARF analysis which contains variable information per fun.
 def process_file(filename):
+    with open_view(filename) as bv:                
+        arch = Architecture['x86_64']
+        print("Step: Binary Ninja")
+        print("Number of functions: ", len(bv.functions))
+        for fun in bv.functions:
+            fun_name = ""
+            for bb in fun:
+                inst_list = list()
+                for inst in bb.get_disassembly_text():
+                    parsed_inst = "" # This is needed in order to avoid adding annotation
+                    annot = False
+                    for token in inst.tokens:
+                        #print(token.text, token.type)
+                        #print("Annot: ", annot)
+                        if token.type == InstructionTextTokenType.AnnotationToken:
+                            annot = True  
+                        elif (token.type == InstructionTextTokenType.EndMemoryOperandToken):
+                            annot = False
+                        elif (annot == True):
+                            continue
+
+                        if (annot == False):
+                            parsed_inst += token.text
+                    parsed_inst = ' '.join(parsed_inst.split())
+                    #print(parsed_inst)
+                    regex = re.search(r'(.*)(?=:)', parsed_inst)
+                    if regex:
+                        #print(inst)
+                        fun_name = regex.group(0)
+                    else:
+                        inst_list.append(parsed_inst)
+                    #print("Parsed: ", parsed_inst)
+                #for parsed_i in inst_list:
+                #    print(parsed_i)
+                bninja_fun_insts[fun_name] = inst_list
+            print("\n")
+
+    
+    for item in bninja_fun_insts:
+        print("Fun: ", item)
+        for inst in bninja_fun_insts[item]:
+            print("\tInst: " , inst)
+
     print('Processing file:', filename)
     with open(filename, 'rb') as f:
         elffile = ELFFile(f)
@@ -47,10 +112,15 @@ def process_file(filename):
                 CU.cu_offset, CU['unit_length']))
 
             # A CU provides a simple API to iterate over all the DIEs in it.
+            local_var_to_locs = list()
+            global_var_to_locs = list()
+            inside_fun = False
             for DIE in CU.iter_DIEs():
+                funname = ""
                 # Go over all attributes of the DIE. Each attribute is an
                 # AttributeValue object (from elftools.dwarf.die), which we
                 # can examine.
+                var_to_loc = tuple()
                 if (DIE.tag == "DW_TAG_subprogram"):
                     lowpc = DIE.attributes['DW_AT_low_pc'].value
                     # DWARF v4 in section 2.17 describes how to interpret the
@@ -68,21 +138,29 @@ def process_file(filename):
                         print('Error: invalid DW_AT_high_pc class:',
                             highpc_attr_class)
                         continue
-                for attr in DIE.attributes.values():
-                    if (DIE.tag == "DW_TAG_subprogram" and
-                        attr.name == "DW_AT_name"):
-                        print("Function name:", DIE.attributes["DW_AT_name"].value.decode(), "\t| Begin: ", hex(lowpc), "\t| End:", hex(highpc))
+                    inside_fun = True
+                    funname = DIE.attributes["DW_AT_name"].value.decode()
+                    print("\n----------------------------------------------------------------------------")
+                    print("Function name:", funname, "\t| Begin: ", hex(lowpc), "\t| End:", hex(highpc))
+                    var_to_loc = (funname, "Function")
+                    local_var_to_locs.append(var_to_loc)
 
+                tuple_exists = False                    
+                varname = ""
+                for attr in DIE.attributes.values():
                     # Check if this DIE is a variable + contains name infomation.
                     if (DIE.tag == "DW_TAG_variable" and
                         attr.name == "DW_AT_name"):
                         print("Variable name:", DIE.attributes["DW_AT_name"].value.decode())
-                    if (DIE.tag == "DW_TAG_formal_parameter" and
-                        attr.name == "DW_AT_name"):
-                        print("Param name:", DIE.attributes["DW_AT_name"].value.decode())
+                        varname = DIE.attributes["DW_AT_name"].value.decode()
+                    #if (DIE.tag == "DW_TAG_formal_parameter" and
+                    #    attr.name == "DW_AT_name"):
+                    #    print("Param name:", DIE.attributes["DW_AT_name"].value.decode())
+                    
                     # Check if this attribute contains location information
+                    
                     if loc_parser.attribute_has_location(attr, CU['version']):
-                        print('   DIE Tag: %s\t attr: %s.' % (DIE.tag,attr.name))
+                        #print('   DIE Tag: %s\t attr: %s.' % (DIE.tag,attr.name))
                         loc = loc_parser.parse_from_attribute(attr,
                                                               CU['version'])
                         # We either get a list (in case the attribute is a
@@ -91,31 +169,105 @@ def process_file(filename):
                         # information).
                         # DW_OP_addr or DW_OP_fbreg
                         if isinstance(loc, LocationExpr):
-                            print('      %s\n' % (
-                                describe_DWARF_expr(loc.loc_expr,
-                                                    dwarfinfo.structs, CU.cu_offset)))
+                            if (DIE.tag == "DW_TAG_variable"):
+                                variable_loc = describe_DWARF_expr(loc.loc_expr,
+                                                        dwarfinfo.structs, CU.cu_offset)
+                                op_fbreg_regex = re.search(r'(?<=DW_OP_fbreg:\s)(.*)(?=\))', variable_loc)
+                                op_addr_regex = re.search(r'(?<=DW_OP_addr:\s)(.*)(?=\))', variable_loc)
+                                if op_fbreg_regex:
+                                    #print('      %s\n' % (variable_loc))
+                                    print("\tLocation:", hex(int(op_fbreg_regex.group(0))), varname)
+                                    var_to_loc = (varname, hex(int(op_fbreg_regex.group(0))))
+                                    tuple_exists = True
+                                elif op_addr_regex:
+                                    print("\tLocation:", op_addr_regex.group(0), varname)
+                                    var_to_loc = (varname, op_addr_regex.group(0))
+                                    tuple_exists = True
+               
                         # LocationEntry (need to parse this properly)
                         elif isinstance(loc, list):
-                            print(show_loclist(loc,
-                                               dwarfinfo,
-                                               '      ', CU.cu_offset))
+                            print(show_loclist(loc,dwarfinfo,'', CU.cu_offset, lowpc, funname))
+                    if (tuple_exists and inside_fun):
+                        local_var_to_locs.append(var_to_loc)
+                if (tuple_exists and inside_fun == False):
+                    global_var_to_locs.append(var_to_loc)
+                
+        # ------- After DIE ------- #   
+        print("\n")             
+        
+        
+        var_list = {}
+        var_list.setdefault("global", [])
+        for item in global_var_to_locs:
+            #print(item)
+            var_list.setdefault("global", []).append(item)
+        
+        fun_name = ""
+        for item in local_var_to_locs:
+            if item[1] == "Function":
+                #print("Function key found")
+                fun_name = item[0]
+                var_list.setdefault(fun_name, [])
+            else:
+                #print(fun_name, item)
+                var_list.setdefault(fun_name, []).append(item)
+                
+        for item in var_list:
+            print(item, var_list[item])
+        
+        for item in dwarf_frame_bases:
+            print(item)
+            for addr_range in dwarf_frame_bases[item]:
+                print("\t",addr_range)
+        
+        for item in dwarf_var_info:
+            print(item)
+            for var_info in dwarf_var_info[funname]:
+                print("\t", var_info)
                         
 
-def show_loclist(loclist, dwarfinfo, indent, cu_offset):
+def show_loclist(loclist, dwarfinfo, indent, cu_offset, entryaddr, funname):
     """ Display a location list nicely, decoding the DWARF expressions
         contained within.
     """
     d = []
+    input_entry = entryaddr
+    index = 0
+    addr_to_exprs = list()
     for loc_entity in loclist:
+        index += 1
         if isinstance(loc_entity, LocationEntry):
-            print("   Entry offset: ", loc_entity.begin_offset, "\t| End offset: ", loc_entity.end_offset, 
-                    "\t| Offset: ", describe_DWARF_expr(loc_entity.loc_expr, dwarfinfo.structs, cu_offset))
-                #d.append('%s <<%s>>' % (
-                #loc_entity,
-                #describe_DWARF_expr(loc_entity.loc_expr, dwarfinfo.structs, cu_offset)))
             
-        #else:
-        #    d.append(str(loc_entity))
+            parse_expr = describe_DWARF_expr(loc_entity.loc_expr, dwarfinfo.structs, cu_offset)
+            #regex = re.search(r'(?<=DW_OP_[a-z,A-Z,0-9]....\s)(?=\((.*)\))|(?<=:\s([0-99]))', parse_expr)
+            offset_diff = loc_entity.end_offset - loc_entity.begin_offset    
+            addr_range = ""            
+            if (index == 1):
+                input_entry += offset_diff
+                #print("   Offset diff: ", offset_diff, "\tSA: ", hex(entryaddr),
+                #    "\tEA: ", hex(input_entry),"\t| Offset: ", parse_expr)
+                addr_range += hex(entryaddr) + "-" + hex(input_entry)
+            else:
+                end_entry = input_entry
+                end_entry += offset_diff
+                #print("   Offset diff: ", offset_diff, "\tSA: ", hex(input_entry),
+                #    "\tEA: ", hex(end_entry),"\t| Offset: ", parse_expr)
+                addr_range += hex(input_entry) + "-" + hex(end_entry)
+                input_entry += offset_diff
+            dw_op_regex = re.search(r'(?<=\(DW_OP_[a-z,A-Z,0-9]....\s\()(.*)(?=\):\s(.*)(?=\)))', parse_expr)
+            addr_to_expr = tuple()
+            if (dw_op_regex):
+                parsed_expr = ""
+                parsed_expr += dw_op_regex.group(1) + "+" + str(int(dw_op_regex.group(2)))
+                #print(addr_range, parsed_expr)
+                addr_to_expr = (addr_range, parsed_expr)
+                addr_to_exprs.append(addr_to_expr)
+    dwarf_frame_bases[funname] = addr_to_exprs
+    #for item in addr_to_exprs:
+    #    print(item)
+           
+
+            
     return '\n'.join(indent + s for s in d)
 
 if __name__ == '__main__':
