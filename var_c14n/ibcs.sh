504251498573ab15f718887692409f8fe52a9ab9
Jae-Won <jjang3@vt.edu>
2024-06-02 00:03:43 -0400

    ibcs.sh cleanup

---
+++ var_c14n/ibcs.sh
@@ -0,0 +1,62 @@
+# This script is used for ARCS
+#!/bin/bash
+
+
+PS3="Select options: "
+input=$1
+
+CFLAGS="-O0 -gdwarf-2"
+
+options=("Build" "Rewrite")
+
+# This is used to setup test path
+grandp_path=$( cd ../../"$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+parent_path=$( cd ../"$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+current_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+useful_path=$parent_path/useful_scripts
+
+ibcs_build_path=${static_path}/build
+ibcs_lib_path=${ibcs_build_path}/lib
+
+ibcs_input_path=${current_path}/input
+
+ibcs_result_path=${current_path}/result
+ibcs_i_result_path=${ibcs_result_path}/$1
+
+rewrite_path=${current_path}/asm_rewriter
+
+build()
+{
+    echo "Build" 
+
+    if [ ! -d "$arcs_result_path" ]; then
+        echo "Result directory doesn't exist"
+        mkdir $ibcs_result_path
+    fi
+
+    if [ ! -d "$arcs_i_result_path" ]; then
+        echo "Input result directory doesn't exist"
+        mkdir $ibcs_i_result_path
+    fi
+    cd ${ibcs_input_path} && make ${input}.out
+}
+
+rewrite()
+{
+    echo "Assembly rewriting the application" 
+    cd ${rewrite_path} && python3 main.py --binary ${input}.out 
+    cd ${ibcs_i_result_path} && make lib && make ${input}.new
+}
+
+while true; do
+    select option in "${options[@]}" Quit
+    do
+        case $REPLY in
+            1) echo "Selected $option"; build; break;;
+            2) echo "Selected $option"; rewrite; break;;
+            $((${#options[@]}+1))) echo "Finished!"; break 2;;
+            *) echo "Wrong input"; break;
+        esac;
+    done
+done
